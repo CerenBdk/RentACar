4dc4efdba0fa09bd604c0ef8ac4d865e3e9f5d5f
CerenBdk <cerenbdk@gmail.com>
2021-02-06 15:38:37 +0300

    Add files via upload

---
+++ Entities/Concrete/Brand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Brand:Base
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Car:Base
+    {
+        public int BrandID { get; set; }
+        public int ColorID { get; set; }
+        public int ModelYear { get; set; }
+        public decimal DailyPrice { get; set; }
+        public string Description { get; set; }
+    }
+}

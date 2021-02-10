3b62d97450936b6d49e6a3903f04316d02411f46
CerenBdk <cerenbdk@gmail.com>
2021-02-10 16:23:54 +0300

    Update ICarDal.cs

--- DataAccess/Abstract/ICarDal.cs
+++ DataAccess/Abstract/ICarDal.cs
@@ -4,12 +4,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities.DTOs;
 
 namespace DataAccess.Abstract
 {
     public interface ICarDal:IEntityRepository<Car>
     {
-        
-        
+        List<CarDetailDto> GetCarDetails();
     }
 }

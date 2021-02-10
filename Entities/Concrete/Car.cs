6cdabc7ba94a5e34822367a460e2e962b9a9a8d3
CerenBdk <cerenbdk@gmail.com>
2021-02-10 16:27:47 +0300

    Update Car.cs

--- Entities/Concrete/Car.cs
+++ Entities/Concrete/Car.cs
@@ -1,4 +1,5 @@
-﻿
+
+using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,10 +8,12 @@ using System.Threading.Tasks;
 
 namespace Entities.Concrete
 {
-    public class Car:Base
+    public class Car:IEntity
     {
+        public int ID { get; set; }
         public int BrandID { get; set; }
         public int ColorID { get; set; }
+        public string Name { get; set; }
         public int ModelYear { get; set; }
         public decimal DailyPrice { get; set; }
         public string Description { get; set; }

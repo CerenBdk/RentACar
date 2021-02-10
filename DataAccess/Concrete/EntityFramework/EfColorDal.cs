1e5aac687d05033edb79a4b90162f86eeaf0a1f6
CerenBdk <cerenbdk@gmail.com>
2021-02-10 16:25:47 +0300

    Update EfColorDal.cs

--- DataAccess/Concrete/EntityFramework/EfColorDal.cs
+++ DataAccess/Concrete/EntityFramework/EfColorDal.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Abstract;
+using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -10,52 +11,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfColorDal:IColorDal
-    {
-        public Color Get(Expression<Func<Color, bool>> filter)
-        {
-            using (RecapProjectDBContext context = new RecapProjectDBContext())
-            {
-                return context.Set<Color>().SingleOrDefault(filter);
-            }
-        }
-
-        public List<Color> GetAll(Expression<Func<Color, bool>> filter = null)
-        {
-            using (RecapProjectDBContext context = new RecapProjectDBContext())
-            {
-                return filter == null ? context.Set<Color>().ToList() : context.Set<Color>().Where(filter).ToList();
-            }
-        }
-
-        public void Add(Color entity)
-        {
-            using (RecapProjectDBContext context = new RecapProjectDBContext())
-            {
-                var newEntity = context.Entry(entity);
-                newEntity.State = EntityState.Added;
-                context.SaveChanges();
-            }
-        }
-
-        public void Update(Color entity)
-        {
-            using (RecapProjectDBContext context = new RecapProjectDBContext())
-            {
-                var updatedEntity = context.Entry(entity);
-                updatedEntity.State = EntityState.Modified;
-                context.SaveChanges();
-            }
-        }
-
-        public void Delete(Color entity)
-        {
-            using (RecapProjectDBContext context = new RecapProjectDBContext())
-            {
-                var deletedEntity = context.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                context.SaveChanges();
-            }
-        }
+    public class EfColorDal: EfEntityRepositoryBase<Color, RecapProjectDBContext>,IColorDal
+    {    
     }
 }

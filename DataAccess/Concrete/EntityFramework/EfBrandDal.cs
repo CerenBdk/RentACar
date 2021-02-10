cd8963f0a9a8ff202fdf995067a0f277077af31f
CerenBdk <cerenbdk@gmail.com>
2021-02-10 16:24:49 +0300

    Update EfBrandDal.cs

--- DataAccess/Concrete/EntityFramework/EfBrandDal.cs
+++ DataAccess/Concrete/EntityFramework/EfBrandDal.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Abstract;
+using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -10,52 +11,8 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfBrandDal:IBrandDal
+    public class EfBrandDal:EfBrandDal:EfEntityRepositoryBase<Brand, RecapProjectDBContext>,IBrandDal
     {
-        public Brand Get(Expression<Func<Brand, bool>> filter)
-        {
-            using (RecapProjectDBContext context = new RecapProjectDBContext())
-            {
-                return context.Set<Brand>().SingleOrDefault(filter);
-            }
-        }
-
-        public List<Brand> GetAll(Expression<Func<Brand, bool>> filter = null)
-        {
-            using (RecapProjectDBContext context = new RecapProjectDBContext())
-            {
-                return filter == null ? context.Set<Brand>().ToList() : context.Set<Brand>().Where(filter).ToList();
-            }
-        }
-
-        public void Add(Brand entity)
-        {
-            using (RecapProjectDBContext context = new RecapProjectDBContext())
-            {
-                var newEntity = context.Entry(entity);
-                newEntity.State = EntityState.Added;
-                context.SaveChanges();
-            }
-        }
-
-        public void Update(Brand entity)
-        {
-            using (RecapProjectDBContext context = new RecapProjectDBContext())
-            {
-                var updatedEntity = context.Entry(entity);
-                updatedEntity.State = EntityState.Modified;
-                context.SaveChanges();
-            }
-        }
-
-        public void Delete(Brand entity)
-        {
-            using (RecapProjectDBContext context = new RecapProjectDBContext())
-            {
-                var deletedEntity = context.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                context.SaveChanges();
-            }
-        }
+        
     }
 }

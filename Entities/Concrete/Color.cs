3f148f561cc325e37dc462e744b16b0920201084
CerenBdk <cerenbdk@gmail.com>
2021-02-10 16:28:04 +0300

    Update Color.cs

--- Entities/Concrete/Color.cs
+++ Entities/Concrete/Color.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,9 @@ using System.Threading.Tasks;
 
 namespace Entities.Concrete
 {
-    public class Color:Base
+    public class Color:IEntity
     {
+        public int ID { get; set; }
         public string Name { get; set; }
     }
 }

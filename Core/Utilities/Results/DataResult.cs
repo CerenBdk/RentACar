bd42686180014e2a04f5e2baa1587e1168c28f24
CerenBdk <cerenbdk@gmail.com>
2021-02-11 17:15:57 +0300

    Add files via upload

---
+++ Core/Utilities/Results/DataResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class DataResult<T>:Result,IDataResult<T>
+    {
+        private T _data;
+        public DataResult(T data, bool success, string message):base(success,message )
+        {
+
+        }
+        public DataResult(T data, bool success):base(success)
+        {
+               this._data = data;
+        }
+        public T Data 
+        { 
+            get { return this._data; } 
+        }
+       
+    }
+}

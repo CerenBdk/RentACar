750b5f7b9ed839a4060c813e90f7b24c6d13b883
CerenBdk <cerenbdk@gmail.com>
2021-02-11 17:14:47 +0300

    Add files via upload

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string AddSingular = " has been added.";
+        public static string UpdateSingular = " has been updated.";
+        public static string DeleteSingular = " has been deleted.";
+        public static string CarNameValidation = "The value entered must be at least 2 characters.";
+    }
+}

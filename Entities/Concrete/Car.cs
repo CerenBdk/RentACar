ea699dabcbd686338d3f7964379ffa22c66a80b5
CerenBdk <cerenbdk@gmail.com>
2021-02-13 01:20:21 +0300

    Update Car.cs

--- Entities/Concrete/Car.cs
+++ Entities/Concrete/Car.cs
@@ -17,5 +17,6 @@ namespace Entities.Concrete
         public int ModelYear { get; set; }
         public decimal DailyPrice { get; set; }
         public string Description { get; set; }
+        public bool IsRented { get; set; }
     }
 }

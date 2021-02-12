c19e22fa8db0d6e73dc2e382334b0c285150ac16
CerenBdk <cerenbdk@gmail.com>
2021-02-13 01:18:00 +0300

    Add files via upload

---
+++ DataAccess/Concrete/EntityFramework/EfCustomerDal.cs
@@ -0,0 +1,33 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCustomerDal:EfEntityRepositoryBase<Customer, RecapProjectDBContext>,ICustomerDal
+    {
+        public List<CustomerDetailDto> GetCustomerDetails()
+        {
+            using (RecapProjectDBContext context = new RecapProjectDBContext())
+            {
+                var result = from u in context.User
+                             join c in context.Customer
+                             on u.ID equals c.UserID
+                             select new CustomerDetailDto
+                            {
+                               FirstName = u.FirstName,
+                               LastName = u.LastName,
+                               Email = u.Email,
+                               CompanyName = c.CompanyName
+                            };
+                return result.ToList();
+            }
+        }
+    }
+}

8090f731cac365a0ffbeb7d25469e411a8b47c67
CerenBdk <cerenbdk@gmail.com>
2021-02-13 01:07:38 +0300

    Add files via upload

---
+++ Business/Abstract/ICustomerService.cs
@@ -0,0 +1,16 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface ICustomerService:IEntityService<Customer>
+    {
+        IDataResult<List<CustomerDetailDto>> GetCustomerDetails();
+    }
+}
---
+++ Business/Abstract/IRentalService.cs
@@ -0,0 +1,16 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IRentalService:IEntityService<Rental>
+    {
+        IDataResult<List<RentalDetailDto>> GetRentalDetails();
+    }
+}

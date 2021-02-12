634de07d6c70958cc31cb34d192a434faf8ac620
CerenBdk <cerenbdk@gmail.com>
2021-02-13 01:18:24 +0300

    Update EfBrandDal.cs

--- DataAccess/Concrete/EntityFramework/EfBrandDal.cs
+++ DataAccess/Concrete/EntityFramework/EfBrandDal.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfBrandDal:EfBrandDal:EfEntityRepositoryBase<Brand, RecapProjectDBContext>,IBrandDal
+    public class EfBrandDal:EfEntityRepositoryBase<Brand, RecapProjectDBContext>,IBrandDal
     {
         
     }

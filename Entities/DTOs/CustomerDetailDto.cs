bdf0d7d161d6c80b297279aca346bc4a41d04076
CerenBdk <cerenbdk@gmail.com>
2021-02-13 01:21:24 +0300

    Add files via upload

---
+++ Entities/DTOs/CustomerDetailDto.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTOs
+{
+    public class CustomerDetailDto:IDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
---
+++ Entities/DTOs/RentalDetailDto.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTOs
+{
+    public class RentalDetailDto:IDto
+    {
+        public int RentalID { get; set; }
+        public string CarName { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+    }
+}

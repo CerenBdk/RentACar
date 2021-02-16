fe594cd1d6d84aeb4cff2f3b4b3da6ab3828da0d
CerenBdk <cerenbdk@gmail.com>
2021-02-16 19:49:21 +0300

    Update EfCustomerDal.cs

--- DataAccess/Concrete/EntityFramework/EfCustomerDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCustomerDal.cs
@@ -16,8 +16,8 @@ namespace DataAccess.Concrete.EntityFramework
         {
             using (RecapProjectDBContext context = new RecapProjectDBContext())
             {
-                var result = from u in context.User
-                             join c in context.Customer
+                var result = from u in context.Users
+                             join c in context.Customers
                              on u.ID equals c.UserID
                              select new CustomerDetailDto
                             {

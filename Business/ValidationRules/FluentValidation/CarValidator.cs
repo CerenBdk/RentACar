5420a54e44dfba68611cfe526013dc9ab1ed3525
CerenBdk <cerenbdk@gmail.com>
2021-02-22 01:51:27 +0300

    Add files via upload

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(x => x.BrandID).NotEmpty();
+            RuleFor(x => x.ColorID).NotEmpty();
+            RuleFor(x => x.Name).NotEmpty(); 
+            RuleFor(x => x.Name).Length(2, 50);
+            RuleFor(x => x.DailyPrice).NotEmpty();
+            RuleFor(x => x.DailyPrice).GreaterThan(0);
+            RuleFor(x => x.ModelYear).NotEmpty();
+            RuleFor(x => x.DailyPrice).NotEmpty();
+        }
+    }
+}

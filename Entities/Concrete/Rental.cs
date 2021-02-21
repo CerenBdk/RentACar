a98dcf30c8d41c4884e636710eb97ffa125a49dd
CerenBdk <cerenbdk@gmail.com>
2021-02-22 01:58:46 +0300

    Add files via upload

---
+++ Entities/Concrete/Rental.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Rental:IEntity
+    {
+        public int ID { get; set; }
+        public int CarID { get; set; }
+        public int CustomerID { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+    }
+}

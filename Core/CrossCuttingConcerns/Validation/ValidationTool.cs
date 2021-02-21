3f1008fb20324400142eec91f8054eafe9619170
CerenBdk <cerenbdk@gmail.com>
2021-02-22 01:52:19 +0300

    Add files via upload

---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+    public class ValidationTool
+    {
+        public static void Validate(IValidator validator, object entity)
+        {
+            var context = new ValidationContext<object>(entity);
+            var result = validator.Validate(context);
+            if (!result.IsValid)
+                throw new ValidationException(result.Errors);
+        }
+    }
+}

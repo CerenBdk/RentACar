2731f3fdeb5676cfefbb102cf30ba0b42c0b8a31
CerenBdk <cerenbdk@gmail.com>
2021-02-22 01:55:49 +0300

    Add files via upload

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DataAccess.Concrete;
+using Business.Concrete;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleManager consoleManager = new ConsoleManager();
+            consoleManager.Dashboard();
+        }
+    }
+}
